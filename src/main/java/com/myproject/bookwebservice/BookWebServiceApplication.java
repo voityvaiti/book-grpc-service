b7f37309a63ae69c8459c4400192062b813ba9d9
voityvaiti <flashlight@i.ua>
2024-03-23 23:26:04 +0200

    Init

---
+++ src/main/java/com/myproject/bookwebservice/BookWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.bookwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWebServiceApplication.class, args);
+	}
+
+}
