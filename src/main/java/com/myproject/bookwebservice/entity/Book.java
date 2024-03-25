060d4bd8e88488ab75c588d8bafcbf4e52caeb1f
voityvaiti <flashlight@i.ua>
2024-03-25 12:39:24 +0200

    Book entity and repository created

---
+++ src/main/java/com/myproject/bookwebservice/entity/Book.java
@@ -0,0 +1,59 @@
+package com.myproject.bookwebservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn) && Objects.equals(quantity, book.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, isbn, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
